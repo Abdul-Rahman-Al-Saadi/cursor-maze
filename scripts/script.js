011cd5ee564c26a26b16c50dc3ac435e5a0c1153
Abdul Rahmna Al Saadi <abedsaadi987@gmail.com>
2024-10-28 23:53:32 +0200

    Finished the game

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,47 @@
-console.log("script connected")
 
 function start() {
-    console.log("function start is working");
+    isGameStarted = true;
+    status.textContent = "Game Started";
 }
 
 function finish() {
-    console.log("function finish is working");
+    if(isGameStarted && !isGameOver){
+        status.textContent = "You Win !!!";
+        isGameOver = true;
+
+        setTimeout(() => {
+            location.reload();
+        }, 2000);
+    }
 }
 
 function play() {
-    console.log("function play is working");
+    if(isGameStarted && !isGameOver){
+        status.textContent = "You Lost :(";
+        isGameOver = true;
+
+        setTimeout(() => {
+            if (confirm("You lost! Would you like to try again?")) {
+                reset(); 
+            } else {
+                status.textContent = "Game Over. Refresh the page to restart."; 
+            }
+        }, 1000);
+    }
+}
+
+function reset(){
+    status.textContent = "Begin by moving your mouse over the 'S'.";
+    isGameOver = false;
+    isGameStarted = false;
 }
 
 var startPosition = document.getElementById("start");
 var endPosition = document.getElementById("end");
 var boundaries = document.querySelectorAll(".boundary");
+let status = document.getElementById("status");
+var isGameStarted = false;
+var isGameOver = false;
 
 startPosition.addEventListener("mouseenter", start);
 endPosition.addEventListener("mouseenter", finish);
